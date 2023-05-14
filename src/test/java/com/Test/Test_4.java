79db06f6b501c76a7daf7a363385afd3c7e56008
GovindPatil75 <mulegr75@gmail.com>
2023-05-14 09:10:10 +0530

    added payment test

---
+++ src/test/java/com/Test/Test_4.java
@@ -0,0 +1,13 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+import com.Utility.BaseClass;
+
+public class Test_4 extends BaseClass{
+
+	@Test
+	public void verifyPaymentTest() {
+		System.out.println("Test");
+	}
+}
